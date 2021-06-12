9257cfebce0505b6fa9bca75ce8caac4db087acc
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2021-06-12 16:41:54 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+ const nextConfig = {
+	/* config options here */
+	experimental: {},
+	future: {
+		webpack5: true
+	}
+      }
+      
+      module.exports = nextConfig
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
