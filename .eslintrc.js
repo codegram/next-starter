603318990b9a6b0e1d1c0605fb1da5314ba23812
Verónica Ojeda <glebapps@gmail.com>
2021-07-26 11:13:28 +0200

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   settings: {
     react: { version: 'detect' },
@@ -46,6 +47,7 @@ module.exports = {
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
   },
   overrides: [
     {
