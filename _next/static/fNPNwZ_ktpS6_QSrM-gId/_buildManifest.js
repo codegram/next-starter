e408e537b8c7659605234b451a67e386f6937fc5
josepjaume <josepjaume@gmail.com>
2021-09-16 17:37:37 +0000

    Deploying to gh-pages from @ codegram/next-starter@e8d2feadbb3b6b5e6b09429b5ee0c0dfafcf6768 🚀

---
+++ _next/static/fNPNwZ_ktpS6_QSrM-gId/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7557b65a543741c5d8e9.css","static/chunks/pages/index-4d496a7ee7f8e86e5385.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/sUroyW4nVpfMlsnO7_oxm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7557b65a543741c5d8e9.css","static/chunks/pages/index-4f688c42ac72362aa1eb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
