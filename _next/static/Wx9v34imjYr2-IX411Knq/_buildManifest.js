7ba1775844d9198357cbdf7086fb1651c9a21cdb
josepjaume <josepjaume@gmail.com>
2021-09-16 17:39:10 +0000

    Deploying to gh-pages from @ codegram/next-starter@2b00567932f991a1cf264faae385fda8e9d8f4d4 🚀

---
+++ _next/static/Wx9v34imjYr2-IX411Knq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7557b65a543741c5d8e9.css","static/chunks/pages/index-bdb8bcaddc0cb17e2763.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
